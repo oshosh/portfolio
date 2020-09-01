f92ee72b79581c71cbd6486edadc4825248014f2
osh <dhtpgus7@gmail.com>
2020-09-01 17:48:59 +0900

    Updates

---
+++ precache-manifest.976128e50b4b4a2e82f923441bfc2057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9114ee2dd344940c0950745ff1fcbf6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fe861a585823d15ea045",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/portfolio/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe861a585823d15ea045",
+    "url": "/portfolio/static/js/main.5ea9e789.chunk.js"
+  },
+  {
+    "revision": "976dd82999372d9d892d",
+    "url": "/portfolio/static/js/runtime-main.5676a5cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
