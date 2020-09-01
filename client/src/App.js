82e77e1099d52b8d9146ae974e416710da4c7c21
osh <dhtpgus7@gmail.com>
2020-09-01 17:52:30 +0900

    dsadasdas

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          오세현 stupid
         </p>
         <a
           className="App-link"
